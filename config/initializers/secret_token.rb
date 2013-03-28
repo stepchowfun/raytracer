4c8150aac3001046958f222b86d658eab30d11e9
Stephan Boyer <stephan@stephanboyer.com>
2013-03-27 21:51:27 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raytracer::Application.config.secret_token = 'd291de3938c7b82ac3be6446c18eee5a164c452f9cd93e91accf73cd24d4da2c47fdcdbe6e024d15336251f910ef5b2d839f5265867942a76b9e549cb735a723'
